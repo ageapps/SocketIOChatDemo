2466649b6abf2d294f183a60690bc1c36450a0fd
ageapps <age.apps.android@gmail.com>
2016-10-07 09:35:25 +0200

    Socket IO basic working, bower, templating started

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+app.io = require('socket.io')();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,18 +21,25 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// static imports
+//  (web).../assets  == /bower_components (server)
+app.use('/assets', express.static(__dirname + '/bower_components'));
+app.use('/socket', express.static(__dirname + '/node_modules/socket.io-client/'));
 
 app.use('/', routes);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handlers
@@ -37,22 +47,32 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+    app.use(function(err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
-  });
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
+// start listen with socket.io
+app.io.on('connection', function(socket){
+  console.log('a user connected');
+
+  socket.on('chat message', function(msg){
+    console.log('chat message: ' + msg);
+    app.io.emit('chat message', msg);
   });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
+var socket = require('socket.io-client')('http://localhost');
+
+
+socket.on('connect', function() {
+    console.log("pp");
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', {
+        title: 'Express'
+    });
 });
+
+router.get('/chat', function(req, res, next) {
+
+    res.render('chat/index', {
+        title: 'Chat'
+    });
+
+});
+
 
 module.exports = router;
